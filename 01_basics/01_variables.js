d93672fc67d83c1fb6fb48bab7885876879d2cf1
dnyanoba04pandage <pandagednyanoba@gmail.com>
2026-09-19 06:38:44 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="srtmu@gmail.com"
+var accountPassword="12345"
+accountCity="mumbai"
+
+accountId=2
+
+console.log(accountId);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)//we are using node js , not browser
+
+//js avoid spaces automatically   code readibility should be high
+
+let name="hitesh"
+let age=23
+let isloggedIn=false
+let state;
+console.table([name,age,isloggedIn])
+
+//data types
+//number 2 to power 53
+//bigint
+//string,boolean=> true/false
+
+//null=> standalone value
+//undefined=>value not assign yet
+//symbol => unique data
+
+//object 
+
+console.log(typeof "vision");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
